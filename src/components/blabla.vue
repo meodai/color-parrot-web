312c2d570eb75902d9af79418e501a9dc77ae471
David Aerne <meodai@gmail.com>
2021-05-06 18:35:43 +0200

    feat(design): FAT LINKS

--- src/components/blabla.vue
+++ src/components/blabla.vue
@@ -11,14 +11,14 @@
           <h2>Usage</h2>
           <p>
             Mention <a href="https://twitter.com/color_parrot">@color_parrot</a>
-            in a reply to a Tweet containing an image and aks "@color_parrot what is this color?"
-            to get a repsonse containg a palette per image.
+            in a reply to a Tweet containing an image and ask "@color_parrot what is this color?"
+            to get a response containing a palette per image.
           </p>
 
           <aside>
             <h3>Specify the number of colors to extract</h3>
             <p>
-              When asking for a color you can specify the amount of colors you would like to get from the bot.
+              When asking for a color you can specify the number of colors you would like to get from the bot.
               (Max. 32) just include a number anywhere in the tweet. Ex: "@color_parrot what are those colors? I need 6 of them..."
             </p>
           </aside>
@@ -26,7 +26,7 @@
           <aside>
             <h3>Want more colors? No problem!</h3>
             <p>
-              Repond with a tweet containg the word "more" to any of the bots reponses to get all the colors.
+              Respond with a tweet containing the word "more" to any of the bot's responses to get all the colors.
             </p>
           </aside>
         </article>
@@ -40,12 +40,12 @@
           </p>
           <h3>Why?</h3>
           <p>
-            I am maintaing an <a href="https://github.com/meodai/color-names">open-source list of names</a>
+            I am maintaining an <a href="https://github.com/meodai/color-names">open-source list of names</a>
             that is <a href="https://github.com/meodai/color-names/issues/98">used by many other apps and websites</a>.
           </p>
           <p>
-            To be able to measure what names are better then others, or that are just plain bad I collect the likes and the reactions
-            to each individual color. So by using the bot you are actively contributing to an open-source project.
+            To be able to measure what names are better than others, or that are just plain bad I collect the likes and the reactions
+            to each color. So by using the bot you are actively contributing to an open-source project.
           </p>
           <h3>Who pays for all that?</h3>
           <p>I am so glad you asked. Hopefully you!</p>
@@ -82,8 +82,8 @@
           </table>
 
           <p>
-            This is not accounting for my own work: Over the past 4 years I have addad an average of
-            19 color names per day. I have spend countless days/weeks/months bulding color tools:
+            This is not accounting for my own work: Over the past 4 years, I have added an average of 20 color names per day.
+            I have spent countless days/weeks/months building color tools:
             <a href="https://farbvelo.elastiq.ch/">FarbVelo</a>, <a href="https://cga.elastiq.ch/">cga</a>, <a href="https://codepen.io/meodai/">And many more...</a>
           </p>
 
@@ -93,12 +93,12 @@
             You will get a special spot on this website and on the <a href="https://github.com/meodai/color-names">github-project page</a>.
           </p>
           <p>
-            A one time donnation also helps a lot. You will be listed on this webpage under a section called "Patreons". The list will be sorted by amount donnated.
-            You can include a link of your choosing.
+            A one-time donation also helps a lot. You will be listed on this webpage under a section called "Patrons".
+            The list will be sorted by the amount donated. You can include a link of your choosing.
           </p>
           <ul>
             <li><a href="https://www.paypal.com/paypalme/elastiq">Paypal</a></li>
-            <li><a href='https://ko-fi.com/A530429S' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://cdn.ko-fi.com/cdn/kofi2.png?v=2' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a></li>
+            <li><a href='https://ko-fi.com/A530429S'>Ko-Fi</a></li>
             <li>ETH Address: 0xA16448c4caE9b95A3783EC29a56AB36D11660061</li>
             <li>Bitccoin Address: 383gmyiQruH5z2JnbsukhSMDDmVJ3Bxogr</li>
           </ul>
